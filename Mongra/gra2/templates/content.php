41eee9f9dbe737ffef5a9ab53d45dd9b6454336d
Mongra <mongra5@wp.pl>
2017-08-03 09:46:12 +0200

    dobra wersja

---
+++ Mongra/gra2/templates/content.php
@@ -0,0 +1,38 @@
+<!-- Dane o użytkowniku -->
+<?php include 'user.php'; ?>
+
+<!-- Lista użytkowników -->
+<?php if ($out['page'] === 'users'): ?>
+    <table>
+    <tr>
+        <td><h1>Nazwa użytkownika</h1></td>
+        <td><h1>Ilość drewna</h1></td>
+    </tr>
+<?php foreach ($out['listUser'] as $klucz => $wartosc): ?>
+    <tr>  
+        <td> <?=  $wartosc['name']; ?></td> 
+        <td> <?=  $wartosc['drewno']; ?></td>        
+    </tr>
+<?php endforeach; ?>
+    </table>
+<?php endif; ?>
+
+<?php if ($out['page'] === 'index'): ?>
+<!-- Strona główna -->
+    <img src="templates/images/osada.jpg" alt="Rysunek osady" id="image_village">
+<?php endif; ?>
+
+<?php if (isset ($out['succecLogout']) && $out['succecLogout'] === true): ?>
+<!-- Wylogowywanie -->
+    Zostałeś pomyślnie wylogowany
+<?php endif; ?>
+
+<?php if ($out['page'] === 'login'): ?>
+<!-- Zalogowany -->
+    Możesz obejrzeć zgromadzone zasoby
+<?php endif; ?>
+
+<?php if ($out['page'] === 'game'): ?>
+<!-- Informacje o grze -->
+    <h2> W grze Osadnicy Online możesz zbudować swoją osadę. Zbieraj surowce, buduj domy i podbijaj wioski innych użytkowników! </h2>
+<?php endif; ?>
---
+++ Mongra/gra2/templates/index.php
@@ -0,0 +1,25 @@
+<?php global $out ?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Zaloguj</title>
+    <link rel="stylesheet" href="/gra2/templates/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Pangolin" rel="stylesheet">
+</head>
+<body>
+    <div id="container">
+        <div id="baner">
+            <b>OSADNICY </b> online
+            <?php include 'login_form.php';?>
+        </div>
+        <?php include 'menu.php' ?>
+        <div id="content">
+            <?php include 'content.php' ?>
+        </div>
+        <div id="footer">Zbuduj swoją osadę już dziś!</div>
+    </div> 
+</body>
+</html>
